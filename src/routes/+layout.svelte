9aea3453e2cfef802ecabdb43099326411517f15
kweithers <kevinweithers@gmail.com>
2023-02-12 16:21:53 -0600

    underline selected menu item

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
     import "./styles.css";
     /** @type {import('./$types').LayoutData} */ export let data;
+    let current;
 </script>
 
 <div class="head-row">
-    <a href="/" class="title">KEKW.pro</a>
+    <a on:click={() => (current = "")} href="/" class="title">KEKW.pro</a>
     <a href="https://www.youtube.com/watch?v=WDiB4rtp1qw&t=41s"
         ><img
             src="https://cdn.betterttv.net/emote/5e9c6c187e090362f8b0b9e8/2x"
@@ -13,7 +14,12 @@
     >
 
     {#each data.sections as section}
-        <a class="menu-item" href="/{section.slug}">{section.title}</a>
+        <a
+            class:selected={current === section.slug}
+            on:click={() => (current = section.slug)}
+            class="menu-item"
+            href="/{section.slug}">{section.title}</a
+        >
     {/each}
 </div>
 
@@ -22,3 +28,9 @@
 <footer>
     This is a hobby project that is in no way affiliated with Twitch.
 </footer>
+
+<style>
+    .selected {
+        text-decoration: underline;
+    }
+</style>
